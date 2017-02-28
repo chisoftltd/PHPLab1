0d759ffcc0c805326d1d628de871565dff63870e
chisoftltd <chisoftltd@gmail.com>
2017-02-28 16:14:26 +0000

    2nd commit

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 <body>
 <p>
     <?php
-    echo "Hello, World!"."\n";
-    echo "Hello," . " " . "world" . "!"."\n";
-    echo 5 * 7;
+    print "Hello, World!"."\n";
+    print "Hello," . " " . "world" . "!"."\n";
+    print 5 * 7;
     $myname = "Frodo Baggins"."\n";
     $myage = 111;
-    echo "My name is" . $myname . "and I am " . $myage;
+    print "My name is" . $myname . "and I am " . $myage;
     ?>
 </p>
 </body>
