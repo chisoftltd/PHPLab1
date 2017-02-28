f0f31318e9cb299da03a4fc92d052f4b61de5d4b
chisoftltd <chisoftltd@gmail.com>
2017-02-28 16:27:59 +0000

    2nd commit

--- index.php
+++ index.php
@@ -5,12 +5,21 @@
 <body>
 <p>
     <?php
-    $name = "Edgar";
-    if ($name == "Simon") {
-        print "I know you!";
-    } else {
-        print "Who are you?";
+    $numberOfHobbits = 2;
+    switch ($numberOfHobbits) {
+        case 1:
+            echo "1 sad hobbit";
+            break;
+        case 2:
+            echo "2 happy hobbits";
+            break;
+        case 3:
+            echo "3 hobbits are a crowd";
+            break;
+        default:
+            echo "All the hobbits have gone home";
     }
+
     ?>
 </p>
 </body>
