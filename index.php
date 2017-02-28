9966bcbebf1b17613c0282ba53e4e384c535b7d7
chisoftltd <chisoftltd@gmail.com>
2017-02-28 16:43:15 +0000

    forloop commit

--- index.php
+++ index.php
@@ -5,11 +5,10 @@
 <body>
 <p>
     <?php
-    $myArray = array("do", "re", "mi"); // declares the array
-    echo $myArray[0]; // outputs "do"
-    $myArray[1] = "la"; // modifies position 1 (re)
-    echo $myArray[1]; // outputs "la"
-    unset($array[2]); // removes the array in position 2
+    $provisionedActivities = array("specs", "mugs", "sausage rolls");
+    foreach ($provisionedActivities as $x) {
+        print "<p>$x</p>";
+    }
     ?>
 </p>
 </body>
