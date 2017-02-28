9396163832de691a60343f8ecc0a3a19ff7c198b
chisoftltd <chisoftltd@gmail.com>
2017-02-28 17:01:47 +0000

    more forloop commit

--- index.php
+++ index.php
@@ -13,15 +13,31 @@
     print "<p><b>As a listen council lets us make changes!</b></p>";
     $provisionedActivities = array("specs", "mugs", "sausage rolls");
     foreach ($provisionedActivities as $x) {
-        if($x == "mugs"){
+        if ($x == "mugs") {
             $x = "hugs";
         }
-        if ($x == "sausage rolls"){
+        if ($x == "sausage rolls") {
             $x = "";
             print "<p>...why</p>";
         }
-            print "<p>$x</p>";
+        print "<p>$x</p>";
     }
+
+    echo "<p>Hello!</p>";
+
+    for ($i = 1; $i < 10; $i++) {
+
+        if ($i / 2 == 0) {
+            echo "<p>Specs</p>";
+        }
+        if ($i / 3 == 0) {
+            echo "<p>Mugs</p>";
+        }
+        if ($i / 4 == 0) {
+            echo "<p>Sausage Rolls</p>";
+        }
+    }
+
     ?>
 </p>
 </body>
