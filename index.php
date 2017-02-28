11bf83fcc3d038c02a9dc215c72c1d235d5408e1
chisoftltd <chisoftltd@gmail.com>
2017-02-28 16:49:56 +0000

    more forloop commit

--- index.php
+++ index.php
@@ -9,6 +9,18 @@
     foreach ($provisionedActivities as $x) {
         print "<p>$x</p>";
     }
+
+    $provisionedActivities = array("specs", "mugs", "sausage rolls");
+    foreach ($provisionedActivities as $x) {
+        if($x == "mugs"){
+            $x = "hugs";
+        }
+        if ($x == "sausage rolls"){
+            $x = "";
+            print "<p>...why</p>";
+        }
+            print "<p>$x</p>";
+    }
     ?>
 </p>
 </body>
